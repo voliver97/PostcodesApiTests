4e529ab81729ed31f98669704426500ead6e087a
Administrator <Administrator@TRWKS025>
2023-10-04 11:14:36 +0100

    Add project files.

---
+++ Models/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Models
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
